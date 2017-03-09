cca1f672c87891cc2564f545863ba31ebe41bc93
Dominik_Hys <dominik.hys@infullmobile.com>
2017-03-09 10:32:44 +0100

    removed additional compiles

--- sample-kotlin/src/test/java/com/infullmobile/android/infullmvp/sample/activity/SampleActivityTest.java
+++ sample-kotlin/src/test/java/com/infullmobile/android/infullmvp/sample/activity/SampleActivityTest.java
@@ -12,9 +12,6 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.robolectric.RobolectricGradleTestRunner;
 
-import dagger.Module;
-import dagger.Provides;
-
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.mock;
 
