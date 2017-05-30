b50aa0e16298717c163a3c17c55335191ce2ba36
jgn <jan.gonera@infullmobile.com>
2017-05-30 12:03:02 +0200

    bumping robolectric to 3.4rc2

--- infullmvp-basetest/src/main/java/com/infullmobile/android/infullmvp/basetest/InFullMvpActivityBaseTest.java
+++ infullmvp-basetest/src/main/java/com/infullmobile/android/infullmvp/basetest/InFullMvpActivityBaseTest.java
@@ -8,7 +8,7 @@ import org.junit.After;
 import org.junit.Before;
 import org.robolectric.Robolectric;
 import org.robolectric.RuntimeEnvironment;
-import org.robolectric.util.ActivityController;
+import org.robolectric.android.controller.ActivityController;
 
 public abstract class InFullMvpActivityBaseTest<
         T extends InFullMvpActivity<PresenterType, PresentedViewType>,
