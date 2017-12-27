28a99e83a9ed9831ca33d45fef800e59076a3373
Andrii Kovalchuk <tieorange@gmail.com>
2017-12-27 11:59:29 +0100

    Merge branch
    Merge branch 'custom-view-support-2' of https://github.com/inFullMobile/InfullMvpAndroid into custom-view-support-2

--- sample-kotlin/src/main/java/com/infullmobile/android/infullmvp/sample/di_components/Components.kt
+++ sample-kotlin/src/main/java/com/infullmobile/android/infullmvp/sample/di_components/Components.kt
@@ -3,7 +3,6 @@ package com.infullmobile.android.infullmvp.sample.di_components
 class Components private constructor() {
 
     companion object {
-
         fun <T> from(activityWithComponent: HasComponent<T>): T = activityWithComponent.component
     }
 }
